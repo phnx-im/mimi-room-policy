ae5c1a42bbcd0fb130228633b86c6691d1f4a936
Timo Kösters <timo@koesters.xyz>
2025-06-03 12:48:41 +0200

    Serialize btreemap without clones thanks to dima

--- src/tls.rs
+++ src/tls.rs
@@ -4,9 +4,7 @@
 
 use std::{fmt, ops::Deref};
 
-use tls_codec::{
-    DeserializeBytes, Error, Serialize, Size, TlsDeserializeBytes, TlsSerialize, TlsSize,
-};
+use tls_codec::{DeserializeBytes, Error, Serialize, Size};
 
 pub mod bool {
     use tls_codec::{DeserializeBytes, Serialize};
@@ -83,43 +81,126 @@ impl Serialize for TlsString {
 }
 
 pub mod btreemap {
-    use std::collections::BTreeMap;
-    use tls_codec::{DeserializeBytes, Serialize, Size};
+    use std::{collections::BTreeMap, io};
+    use tls_codec::{
+        vlen::{read_length, write_length},
+        DeserializeBytes, Serialize, Size,
+    };
 
-    pub fn tls_serialize<K, V, W: std::io::Write>(
+    pub fn tls_serialized_len<K, V>(v: &BTreeMap<K, V>) -> usize
+    where
+        K: Size,
+        V: Size,
+    {
+        let content_len = v
+            .iter()
+            .map(|(k, v)| k.tls_serialized_len() + v.tls_serialized_len())
+            .sum();
+        let len_len = write_length(&mut io::empty(), content_len).unwrap_or(0);
+        content_len + len_len
+    }
+
+    pub fn tls_serialize<K, V, W>(
         v: &BTreeMap<K, V>,
         writer: &mut W,
     ) -> Result<usize, tls_codec::Error>
     where
-        K: std::fmt::Debug + Clone + Serialize + DeserializeBytes,
-        V: std::fmt::Debug + Clone + Serialize + DeserializeBytes,
+        K: Serialize,
+        V: Serialize,
+        W: io::Write,
     {
-        let vec = v
+        // We need to pre-compute the length of the content.
+        // This requires more computations but the other option would be to buffer
+        // the entire content, which can end up requiring a lot of memory.
+        let content_length = v
             .iter()
-            .map(|(k, v)| (k.clone(), v.clone()))
-            .collect::<Vec<_>>();
-        vec.tls_serialize(writer)
+            .map(|(k, v)| k.tls_serialized_len() + v.tls_serialized_len())
+            .sum();
+        let len_len = write_length(writer, content_length)?;
+
+        // Serialize the elements
+        #[cfg(debug_assertions)]
+        let mut written = 0;
+        for (k, v) in v.iter() {
+            #[cfg(debug_assertions)]
+            {
+                written += k.tls_serialize(writer)?;
+                written += v.tls_serialize(writer)?;
+            }
+            #[cfg(not(debug_assertions))]
+            {
+                k.tls_serialize(writer)?;
+                v.tls_serialize(writer)?;
+            }
+        }
+        #[cfg(debug_assertions)]
+        if written != content_length {
+            return Err(tls_codec::Error::LibraryError);
+        }
+
+        Ok(content_length + len_len)
     }
+
     pub fn tls_deserialize_bytes<K, V>(
-        bytes: &[u8],
+        mut bytes: &[u8],
     ) -> Result<(BTreeMap<K, V>, &[u8]), tls_codec::Error>
     where
-        K: std::fmt::Debug + Clone + Serialize + DeserializeBytes + Ord,
-        V: std::fmt::Debug + Clone + Serialize + DeserializeBytes,
+        K: DeserializeBytes + Ord,
+        V: DeserializeBytes,
     {
-        let (val, rest) = <Vec<(K, V)>>::tls_deserialize_bytes(bytes)?;
-        let btreemap = val.into_iter().collect::<BTreeMap<K, V>>();
-        Ok((btreemap, rest))
+        let (len, len_len) = read_length(&mut bytes)?;
+        if len == 0 {
+            return Ok((BTreeMap::new(), bytes));
+        }
+
+        let mut result = BTreeMap::new();
+        let mut read = len_len;
+        while (read - len_len) < len {
+            let (key, key_remainder) = K::tls_deserialize_bytes(bytes)?;
+            bytes = key_remainder;
+            read += key.tls_serialized_len();
+
+            let (value, value_remainder) = V::tls_deserialize_bytes(bytes)?;
+            bytes = value_remainder;
+            read += value.tls_serialized_len();
+
+            result.insert(key, value);
+        }
+        Ok((result, bytes))
     }
-    pub fn tls_serialized_len<K, V>(v: &BTreeMap<K, V>) -> usize
-    where
-        K: std::fmt::Debug + Clone + Serialize + DeserializeBytes,
-        V: std::fmt::Debug + Clone + Serialize + DeserializeBytes,
-    {
-        let vec = v
-            .iter()
-            .map(|(k, v)| (k.clone(), v.clone()))
-            .collect::<Vec<_>>();
-        vec.tls_serialized_len()
+
+    #[cfg(test)]
+    mod tests {
+        use crate::tls::TlsString;
+
+        use super::*;
+
+        #[test]
+        fn test_tls_serde_empty_btreemap() {
+            let map: BTreeMap<u64, TlsString> = Default::default();
+
+            let mut buf = Vec::new();
+            let len = tls_serialize(&map, &mut buf).unwrap();
+            assert_eq!(len, 1);
+            let (map2, remainder) = tls_deserialize_bytes(&buf).unwrap();
+            assert_eq!(map, map2);
+            assert_eq!(remainder.len(), 0);
+        }
+
+        #[test]
+        fn test_tls_serde_btreemap() {
+            let mut map: BTreeMap<u64, TlsString> = Default::default();
+            map.insert(1u64, TlsString("hello".to_owned()));
+            map.insert(3u64, TlsString("world".to_owned()));
+
+            let mut buf = Vec::new();
+            let len = tls_serialize(&map, &mut buf).unwrap();
+            // varint(4) + 1 + len("hello") + bytes("hello") + 3 + len("world") + bytes("world")
+            // 1 + 8 + 1 + 5 + 8 + 1 + 5
+            assert_eq!(len, 29);
+            let (map2, remainder) = tls_deserialize_bytes(&buf).unwrap();
+            assert_eq!(map, map2);
+            assert_eq!(remainder.len(), 0);
+        }
     }
 }
